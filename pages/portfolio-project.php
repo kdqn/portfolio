d09c8a980203172baa8a7976e8e57da542e45e2b
CaydenLaptop <cayden.odegard@gmail.com>
2024-04-30 17:48:17 -0500

    css

---
+++ pages/portfolio-project.php
@@ -0,0 +1,34 @@
+<?php
+$config = parse_ini_file('../config.ini', true);
+$environment = $config['ENVIRONMENT'];
+$URL_BASE = $config[$environment]['root'];
+define('URL_ROOT', "$URL_BASE");
+define('APP_ROOT', dirname(__FILE__,2));
+include_once(APP_ROOT . '/services/database.controller.php');
+
+//Pull database credentials from config.ini
+$user = $config[$environment]['user'];
+$pass = $config[$environment]['pass'];
+$host = $config[$environment]['host'];
+$name = $config[$environment]['name'];
+
+try
+{
+	$conn = new PDO("mysql:host=$host;dbname=$name", $user, $pass);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $database = new DatabaseService($conn);
+} 
+catch(PDOException $e)
+{
+	echo "Connection failed: " . $e->getMessage();
+}
+
+if (isset($database))
+{
+  $controller = new DatabaseController($database);
+  $controller->indexPage('About');
+  $controller->insertImage('../media/mypicture.png');
+  include_once(APP_ROOT . '/views/footer.view.php');
+}
+$conn = null;
+?> 

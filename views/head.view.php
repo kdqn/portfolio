7f757e9eeca4f0f59af717d0d77b78d1c042b6a3
Cayden <cayden.odegard@gmail.com>
2024-11-01 22:03:03 -0500

    testing

--- views/head.view.php
+++ views/head.view.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php echo $data[0]['page'] ?></title>
-	<link rel="icon" type="image/png" href="media\favicon_io\favicon.ico">
+	<link rel="icon" type="image/png" href="media\favicon.ico">
 </head>
 <body>
 <?php include_once('header.view.php')?>

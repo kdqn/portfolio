f6f8710c725c01a0ceff52deb4596cd4b47d16de
Cayden <cayden.odegard@gmail.com>
2024-11-04 14:07:07 -0600

    tweaks

--- views/head.view.php
+++ views/head.view.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php echo $data[0]['page'] ?></title>
-	<link rel="icon" type="image/png" href="<?php echo URL_ROOT . '/media/favicon.io' ?>">
+	<link rel="icon" type="image/png" href="<?php echo URL_ROOT . '../media/favicon.io' ?>">
 </head>
 <body>
 <?php include_once('header.view.php')?>

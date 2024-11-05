d8481cd9678e2644097dc0fbdc0c6cea0c115cc4
Cayden <cayden.odegard@gmail.com>
2024-11-05 15:35:15 -0600

    tweaks

--- views/footer.view.php
+++ views/footer.view.php
@@ -1,5 +1,6 @@
+</body>
 <footer><b>Cayden Odegard - Computer Science Major - BSU </b><br> <a href="mailto:cayden.odegard@gmail.com" target="_blank">cayden.odegard@gmail.com</a> | <a href="mailto:vp1139go@go.minnstate.edu" target="_blank">vp1139go@go.minnstate.edu</a>
 </footer>
 	<!-- <script src="script.js"></script> -->
-</body>
+
 </html>

b21840262ab559c63315d16467ddca6770e962c5
Cayden <cayden.odegard@gmail.com>
2024-12-19 14:56:54 -0600

    set redirect to caydenodegard.com

--- views/head.view.php
+++ views/head.view.php
@@ -9,6 +9,6 @@
 </head>
 <body>
 <?php include_once('header.view.php')?>
-<?php header('Location: http://www.new-website.com');
+<?php header('Location: http://caydenodegard.com/MyPortfolio');
   exit; 
   ?>
